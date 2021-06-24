7f13737d79a462c7fdf15074626203eb41f9f953
vgvm <luckybit4755@gmail.com>
2021-06-24 18:35:18 -0400

    reorganize files and code

--- glsl/include/marcher.glsl
+++ glsl/include/marcher.glsl
@@ -1,6 +1,17 @@
-//define: const vec3 MARCH = vec3( .0, .001, 99. );
+#ifndef SET_MARCH
+#define SET_MARCH
+const vec3 MARCH = vec3( .0, .001, 199. );
+#endif
 
+/////////////////////////////////////////////////////////////////////////////
+
+// defines the scene for march function
+// @param p is the current point
+// @return d.x is the "material index" (reserve 0 for "no hit")
+// @return d.y is the distance from p to the closest object in the scene
 vec2 map( vec3 p );
+
+/////////////////////////////////////////////////////////////////////////////
 
 vec2 march( vec3 eye, vec3 direction ) { 
     vec2 total = vec2(.0), now;
@@ -22,13 +33,4 @@ float map1( vec3 p ) {
 vec3 mapNormal(vec3 p,float d) {
     mat3 k = mat3(p,p,p) - mat3(MARCH.y*d);
     return normalize(map1(p) - vec3(  map1(k[0]),  map1(k[1]),  map1(k[2])) );
-}
-
-vec2 minnow( vec2 current, vec2 candidate ) {
-  return current.y < candidate.y ? current : candidate;
-  return mix( current, candidate, step( candidate.y, current.y ) ); // idk, why this doesn't work 
-}
-
-vec2 minnow( vec2 current, float id, float distance ) {
-  return minnow( current, vec2( id, distance ));
 }
