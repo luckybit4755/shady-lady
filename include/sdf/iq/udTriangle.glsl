14935e63206875a0c70eef5be12ad578c7c708eb
vgvm <luckybit4755@gmail.com>
2021-12-21 19:46:34 -0500

    for triangle meshes

---
+++ include/sdf/iq/udTriangle.glsl
@@ -0,0 +1,23 @@
+#include dots.glsl
+
+// The MIT License ; Copyright © 2013 Inigo Quilez ; see COPYRIGHT file in this directory
+// https://iquilezles.org/www/articles/distfunctions/distfunctions.htm
+float udTriangle( vec3 p, vec3 a, vec3 b, vec3 c )
+{
+  vec3 ba = b - a; vec3 pa = p - a;
+  vec3 cb = c - b; vec3 pb = p - b;
+  vec3 ac = a - c; vec3 pc = p - c;
+  vec3 nor = cross( ba, ac );
+
+  return sqrt(
+    (sign(dot(cross(ba,nor),pa)) +
+     sign(dot(cross(cb,nor),pb)) +
+     sign(dot(cross(ac,nor),pc))<2.0)
+     ?
+     min( min(
+     dot2(ba*clamp(dot(ba,pa)/dot2(ba),0.0,1.0)-pa),
+     dot2(cb*clamp(dot(cb,pb)/dot2(cb),0.0,1.0)-pb) ),
+     dot2(ac*clamp(dot(ac,pc)/dot2(ac),0.0,1.0)-pc) )
+     :
+     dot(nor,pa)*dot(nor,pa)/dot2(nor) );
+}
